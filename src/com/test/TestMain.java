77b4eb0e65eb8377648cf2486ea1b2e4daf36ce7
baki-511 <johanbaki381@gmail.com>
2023-01-20 11:40:37 +0530

    old hibernate project for testing

---
+++ src/com/test/TestMain.java
@@ -0,0 +1,25 @@
+package com.test;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class TestMain {
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();
+		Transaction tx = session.beginTransaction();
+		
+		Student st = new Student();
+		st.setId(101);
+		st.setFirstName("Surendra");
+		st.setLastName("Pal");
+		session.save(st);
+		tx.commit();
+		session.close();
+		System.out.println("Data inserted Successfully....");
+	}
+}
